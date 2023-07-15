cff2d4446fac83f5ea36dab2511b6027af4a8e76
Precious <preciouskay4real@gmail.com>
2023-07-15 15:03:22 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a function that prints base 10 numbers
+ * DESRIPTION: prints from 0-9
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+
+		printf("\n");
+		return (0);
+}
