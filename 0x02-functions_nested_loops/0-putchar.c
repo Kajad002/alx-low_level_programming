cc5e87058c0dab7cf0b06ac4ff57be6beaf1be82
Precious <preciouskay4real@gmail.com>
2023-07-18 08:42:02 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a function that prints _putchar
+ * DESCRIPTION: Do not use printf
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char _putchar;
+
+	{
+		putchar(_putchar);
+	}
+		putchar('\n');
+		return (0);
+}
