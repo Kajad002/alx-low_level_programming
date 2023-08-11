a8354d0864bd52aa2f2123b00e08f5ad9975a1f1
Precious <preciouskay4real@gmail.com>
2023-08-11 05:11:28 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,20 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: function parameter
+ * Return: 98 when failed.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		printf("Memory allocation failed\n");
+		exit(98);
+	}
+	return (ptr);
+}
