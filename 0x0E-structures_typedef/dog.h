08834cd6978a1f34af1ca9e42256527e6cdeb3b4
Precious <preciouskay4real@gmail.com>
2023-08-14 09:52:57 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type struct dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: defines a new struct, with three members
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
