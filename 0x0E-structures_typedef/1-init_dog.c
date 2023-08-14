bc78481fe5133e541529479a5438e669f1b42336
Precious <preciouskay4real@gmail.com>
2023-08-14 12:18:03 -0700

    1. A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @name:first member
+ * @age:second member
+ * @owner:third member
+ * @d:pointer
+ *
+ * Description: a function that initializes a variable of type struct dog,
+ * which takes three arguments
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
