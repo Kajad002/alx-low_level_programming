9438cdbe7463478a26df46285b600e067e91744b
Precious <preciouskay4real@gmail.com>
2023-09-04 16:41:50 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#define BUFSIZE 1024
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 97, 98, 99 or 100 on error
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to;
+	ssize_t n;
+	char buf[BUFSIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		return (97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		return (99);
+	}
+	while ((n = read(fd_from, buf, BUFSIZE)) > 0)
+		if (write(fd_to, buf, n) != n)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			return (99);
+		}
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		return (98);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		close(fd_to);
+		return (100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		return (100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BUFFER_SIZE 1024
 
 #include <unistd.h>
 #include <fcntl.h>
@@ -7,6 +8,7 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
