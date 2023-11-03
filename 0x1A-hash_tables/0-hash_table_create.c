47071d64575217f42666e9f11e1240122238fdd7
Precious <preciouskay4real@gmail.com>
2023-11-03 07:54:22 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,26 +11,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
-    unsigned long int i;
+	hash_table_t *new_table;
+	unsigned long int i;
 
-    if (size < 1) return NULL; /* Invalid size */
+	if (size < 1) return NULL; /* Invalid size */
 
-    /* Attempt to allocate memory for the hash_table structure */
-    if ((new_table = malloc(sizeof(hash_table_t))) == NULL) {
-        return NULL;
-    }
+	/* Attempt to allocate memory for the hash_table structure */
+	if ((new_table = malloc(sizeof(hash_table_t))) == NULL) {
+		return NULL;
+	}
 
-    /* Attempt to allocate memory for the table itself */
-    if ((new_table->array = malloc(sizeof(void *) * size)) == NULL) {
-        return NULL;
-    }
+	/* Attempt to allocate memory for the table itself */
+	if ((new_table->array = malloc(sizeof(void *) * size)) == NULL) {
+		return NULL;
+	}
 
-    /* Initialize the elements of the array */
-    for(i=0; i<size; i++) new_table->array[i] = NULL;
+	/* Initialize the elements of the array */
+	for(i=0; i<size; i++) new_table->array[i] = NULL;
 
-    /* Set the table's size */
-    new_table->size = size;
+	/* Set the table's size */
+	new_table->size = size;
 
-    return new_table;
+	return new_table;
 }
